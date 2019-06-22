2cc9175af352ec06a6bb3a085c0b11c778f37e41
Shtukarev Igor <shtukarevis@mail.dev.local>
2019-06-22 20:41:09 +0300

    Рисую рандомные маршруты

--- app/src/app/map/map.component.ts
+++ app/src/app/map/map.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, ViewChild, ElementRef, AfterViewInit, Input } from '@angular/core';
 import { renderMap, HereMap } from '../utils/map';
-import { Order } from '../utils/orders';
+import { Order, IOrder, Coordinates } from '../utils/orders';
 
 @Component({
   selector: 'app-map',
@@ -8,19 +8,24 @@ import { Order } from '../utils/orders';
   styleUrls: ['./map.component.css']
 })
 export class MapComponent implements OnInit, AfterViewInit {
-  orders: Order[];
+  orders: IOrder[] = [];
   private _map: HereMap;
   @ViewChild('map', { static: false }) el: ElementRef;
-  constructor() { }
+  constructor() {
+   }
 
   ngOnInit() {
-
   }
 
   ngAfterViewInit() {
+    for(var i = 0; i < 10; i++)
+    this.orders.push({ 
+      start :  new Coordinates({ lat: Math.random() * 50, lng: Math.random() * 50 }), 
+      end : new Coordinates({ lat : Math.random() * 50, lng : Math.random() * 50 }), 
+      id : '0'});
     this._map = renderMap(this.el.nativeElement);
     this.orders.forEach((item) => {
-      this._map.showRout(item.start, item.end);
+      this._map.showRoute(item.start, item.end);
     })
 
   }
--- app/src/app/utils/orders.ts
+++ app/src/app/utils/orders.ts
@@ -3,16 +3,30 @@ export enum OrderStatus {
     InProgress,
     Wait
 }
+export interface IOrder {
+    start: Coordinates;
+    end: Coordinates;
+    id: string;
+}
 
-export class Coordinates {
+export interface ICoordinate {
+    lat: number;
+    lng: number;
+}
+
+export class Coordinates implements ICoordinate {
+    constructor(coordinate: ICoordinate = { lat: 0, lng: 0 }) {
+        this.lat = coordinate.lat;
+        this.lng = coordinate.lng;
+    }
     lat: number;
     lng: number;
     toString() {
-        return [this.lat, this.lng].join(', ');
+        return [this.lat, this.lng].join(',');
     }
 }
 
-export class Order {
+export class Order implements IOrder {
     start: Coordinates;
     end: Coordinates;
     id: string;
